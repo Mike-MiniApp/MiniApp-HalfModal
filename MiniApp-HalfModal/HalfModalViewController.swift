a0417ce753fe6cff8c6f42698e08ddee80193d43
mikekondo <foad5969@gmail.com>
2022-04-20 16:35:21 +0900

    動作確認完了

---
+++ MiniApp-HalfModal/HalfModalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HalfModalViewController.swift
+//  MiniApp-HalfModal
+//
+//  Created by 近藤米功 on 2022/04/20.
+//
+
+import UIKit
+
+class HalfModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- MiniApp-HalfModal/ViewController.swift
+++ MiniApp-HalfModal/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let halfModalVC = segue.destination
+        if let sheet = halfModalVC.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            // モーダル出現後も親ビュー操作可能にする
+            sheet.largestUndimmedDetentIdentifier = .medium
+            // 角丸の半径を変更する
+            sheet.preferredCornerRadius = 40.0
+            // グラバー(上についてる横棒）表示する
+            sheet.prefersGrabberVisible = true
+        }
+
+    }
+
+    @IBAction func tappedNextButton(_ sender: Any) {
+        performSegue(withIdentifier: "HalfModalSegue", sender: nil)
+    }
 
 
 }
